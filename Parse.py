5ea4ba68e11dbba32604a1ee4c1110d3af83e760
Sai Akhil <akhiltejamekala@gmail.com>
2021-08-19 20:52:03 +0530

    Feature partial progress commit

---
+++ Parse.py
@@ -0,0 +1,107 @@
+import os
+import shutil
+
+DirVerify = True
+
+def isQuant(dirName):
+	if("Math" in dirName):
+		return False
+	Maths = ['Algebra', 'Data Interpretation', 'Counting', 'Fraction', 'Geometry', 'Integer', 'Percents', 'Powers_Roots', 'Probability', 'Statistics']
+	for elem in Maths:
+		if elem in dirName:
+			return True
+	return	False
+
+def isVerbal(dirName):
+	if("Verbal" in dirName):
+		return False
+	Verbal = ['RC', 'SE', 'TC 1_2', 'TC 3', 'Word Problems']
+	for elem in Verbal:
+		if elem in dirName:
+			return True
+	return	False
+
+def get_immediate_subdirectories(a_dir):
+    return [name for name in os.listdir(a_dir)
+            if os.path.isdir(os.path.join(a_dir, name))]
+
+def get_files_directory(destdir):
+	return [ f for f in os.listdir(destdir) if os.path.isfile(os.path.join(destdir,f)) ]
+
+def buildFolderStructure(oldDir, newDir, QuesDir, AnsDir):
+	if os.path.exists(QuesDir):
+		shutil.rmtree(QuesDir)
+	os.makedirs(QuesDir)
+	if os.path.exists(AnsDir):
+		shutil.rmtree(AnsDir)
+	os.makedirs(AnsDir)
+	dirnames = get_immediate_subdirectories(oldDir)
+	counter=0
+	for dirname in dirnames:
+		root = os.path.join(oldDir, dirname)
+		filenames = get_files_directory(os.path.join(oldDir, dirname))
+		counter += len(filenames)
+		for filename in filenames:
+			newSubDir = ""
+			# copy file accordingly
+			if "Capture" in filename or ("Problem" in filename and ".webm" not in filename):
+				newSubDir = os.path.join(QuesDir, dirname)
+				if not os.path.exists(newSubDir):
+					os.makedirs(newSubDir)
+				shutil.copy2(os.path.join(root, filename), newSubDir)
+			else:
+				newSubDir = os.path.join(AnsDir, dirname)
+				if not os.path.exists(newSubDir):
+					os.makedirs(newSubDir)
+				shutil.copy2(os.path.join(root,filename), newSubDir)
+			print newSubDir
+	return counter
+
+def directoryParse(source, Quant, Verbal, total):
+	print source
+	mainDirNames = get_immediate_subdirectories(source)
+	mainRoot = source
+	dirSize = len(mainDirNames)
+	boolArr = [False for i in range(dirSize)]
+	counter = 0
+	for dirName in mainDirNames:
+		# Quant difference
+		if(isQuant(dirName)):
+			boolArr[counter] = True
+			localQuesfolder = 'Math\\'+dirName+'\Question'
+			localAnsfolder = 'Math\\'+dirName+'\Answer'
+			oldDir = os.path.join(mainRoot, dirName)
+			newDir = os.path.join(mainRoot, 'Math\\'+dirName)
+			QuesDir = os.path.join(mainRoot, localQuesfolder)
+			AnsDir = os.path.join(mainRoot, localAnsfolder)
+			Quant.append(buildFolderStructure(oldDir, newDir, QuesDir, AnsDir))
+		# verbal to-do
+		if(isVerbal(dirName)):
+			boolArr[counter] = True
+			localQuesfolder = 'Verbal\\'+dirName+'\Question'
+			localAnsfolder = 'Verbal\\'+dirName+'\Answer'
+			oldDir = os.path.join(mainRoot, dirName)
+			newDir = os.path.join(mainRoot, 'Verbal\\'+dirName)
+			QuesDir = os.path.join(mainRoot, localQuesfolder)
+			AnsDir = os.path.join(mainRoot, localAnsfolder)
+			buildFolderStructure(oldDir, newDir, QuesDir, AnsDir)
+			Verbal.append(buildFolderStructure(oldDir, newDir, QuesDir, AnsDir))
+		counter += 1
+	for elem in boolArr:
+		if elem is False:
+			DirVerify = False
+			return
+
+def main():
+	Quant=[]
+	Verbal=[]
+	Total=[]
+	path = "E:\MS\GRE Prep Tool"
+	directoryParse(".\Magoosh GRE Package 2020", Quant, Verbal, Total)
+	if DirVerify is False:
+		print "Failed in Directory parsing itself!!"
+	else:
+		print "All are fine!!"
+
+if __name__ == '__main__':
+	main()
--- hello.py
+++ hello.py
@@ -1,5 +1,74 @@
 from flask import Flask, redirect, url_for, render_template, request
+from random import randrange
 app = Flask(__name__)
+
+mathTags = ["Algebra", "Coordinate Geometry", "Counting", "Data Interpretation", "Fraction", "Geometry", "Integer Properties", "Percents and Ratios", "Probability", "Statistics"]
+AlgebraQuestions = []
+CoGeoQuest	= []
+CntQuest = []
+DataInterQuest = []
+FrctQuest = []
+GeoQuest = []
+IntPropQuest = []
+PerRatQuest = []
+ProbQuest = []
+StatsQuest = []
+
+def get_immediate_subdirectories(a_dir):
+    return [name for name in os.listdir(a_dir)
+            if os.path.isdir(os.path.join(a_dir, name))]
+
+# init the arrays with false based on the question count
+def initializeLists():
+	source = "static/Math/"
+	# for each tag list do this
+	for name in os.listdir(source):
+		if os.path.isdir(os.path.join(a_dir, name)):
+			source += name
+			source += "/Question"
+			mainDirNames = get_immediate_subdirectories(source)
+			dirSize = len(mainDirNames)
+			print name
+			if "Algebra" in name:
+				for n in range(dirSize):
+					AlgebraQuestions.append(false)
+					break
+			if "Coordinate Geometry" in name:
+				for n in range(dirSize):
+					CoGeoQuest.append(false)
+					break
+			if "Counting" in name:
+				for n in range(dirSize):
+					CntQuest.append(false)
+					break
+			if "Data Interpretation" in name:
+				for n in range(dirSize):
+					DataInterQuest.append(false)
+					break
+			if "Fraction" in name:
+				for n in range(dirSize):
+					FrctQuest.append(false)
+					break
+			if "Geometry" in name:
+				for n in range(dirSize):
+					GeoQuest.append(false)
+					break
+			if "Integer Properties" in name:
+				for n in range(dirSize):
+					IntPropQuest.append(false)
+					break
+			if "Percents and Ratios" in name:
+				for n in range(dirSize):
+					PerRatQuest.append(false)
+					break
+			if "Probability" in name:
+				for n in range(dirSize):
+					ProbQuest.append(false)
+					break
+			if "Statistics" in name:
+				for n in range(dirSize):
+					StatsQuest.append(false)
+					break
 
 def home_page():
 	return "<p> This is being used to test the decorator 'add_url_rule' </p>"
@@ -23,19 +92,71 @@ def hello_user(name):
 def hello_name():
 	return render_template('hello.html')
 
+def GetQuestion(tag):
+	if "Math" in tag:
+		val = randrange(len(mathTags))
+		return GetQuestion(mathTags[val])
+	if "Algebra" in name:
+	for n in range(dirSize):
+		AlgebraQuestions.append(false)
+		break
+	if "Coordinate Geometry" in name:
+	for n in range(dirSize):
+		CoGeoQuest.append(false)
+		break
+	if "Counting" in name:
+	for n in range(dirSize):
+		CntQuest.append(false)
+		break
+	if "Data Interpretation" in name:
+	for n in range(dirSize):
+		DataInterQuest.append(false)
+		break
+	if "Fraction" in name:
+	for n in range(dirSize):
+		FrctQuest.append(false)
+		break
+	if "Geometry" in name:
+	for n in range(dirSize):
+		GeoQuest.append(false)
+		break
+	if "Integer Properties" in name:
+	for n in range(dirSize):
+		IntPropQuest.append(false)
+		break
+	if "Percents and Ratios" in name:
+	for n in range(dirSize):
+		PerRatQuest.append(false)
+		break
+	if "Probability" in name:
+	for n in range(dirSize):
+		ProbQuest.append(false)
+		break
+	if "Statistics" in name:
+	for n in range(dirSize):
+		StatsQuest.append(false)
+		break
+		#randomly choose based on the selected tag
+
+	return imgUrl
+
 @app.route('/result', methods=['POST','GET'])
 def result():
 	if request.method == 'POST':
 		question = request.form['reply']
 		if question == "Question":
-			return render_template("question.html", question=question)
+			imgUrl = GetQuestion(tag)
+			#imgUrl ='static/Math/Algebra/Question/1/Capture.jpg'
+			#Url = url_for('static', filename={{imgUrl}})
+			return render_template("question.html", Value=imgUrl)
 		if question == "Video":
 			return render_template("video.html")
 		if question == "Answer":
 			return render_template("image.html")
-			
+
 @app.route('/PrepTool')
 def prep_tool():
+	initializeLists()
 	return render_template('index.html')
 
 @app.route('/')
